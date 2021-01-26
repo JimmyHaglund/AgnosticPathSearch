37c6143666e3d6fbd7d332623149a77b66510eb1
Jimmy <Jimmyhaglund@gmail.com>
2021-01-26 16:37:10 +0100

    Add tests for correct return graph count and wether it contains edge coordinates.

--- AgnosticSearch/AgnosticSearch_IntegrationTest/AgnosticSearchTest.cs
+++ AgnosticSearch/AgnosticSearch_IntegrationTest/AgnosticSearchTest.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Collections;
 
 namespace AgnosticSearchTest {
-    public class When_Searching_Path_Of_10_Elements {
+    public class When_Searching_Path_Of_10_Elements_In_Graph_With_Int_Coordinates_Ranging_From_0_To_10 {
         MockPathMaker _pathMaker;
         MockGraph _graph;
         PossiblePaths<int, MockNode> _finder;
+        int _startKey = 5;
         [SetUp]
         public void Setup() {
             _pathMaker = new MockPathMaker();
@@ -17,26 +18,56 @@ namespace AgnosticSearchTest {
 
         [Test]
         public void Start_Node_Should_Be_In_Graph() {
-            var startKey = 5;
-            var path = _finder.Find(_pathMaker, startKey, SearchBlocked, _graph);
-            Assert.NotNull(path.GetNode(startKey));
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            Assert.NotNull(path.GetNode(_startKey));
+        }
+
+        
+
+        [Test]
+        public void Result_Should_Contain_10_Elements() {
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            var count = 0;
+            foreach (var node in path) count++;
+            Assert.AreEqual(10, count);
+
         }
 
         [Test]
-        public void Key_Outside_Range_Should_Not_Be_In_Graph() {
-            var startKey = 5;
-            var outsideRangeKey = -1;
-            var path = _finder.Find(_pathMaker, startKey, SearchBlocked, _graph);
-            Assert.Null(path.GetNode(outsideRangeKey));
+        public void Result_Should_Contain_9() {
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            Assert.NotNull(path.GetNode(9));
+
         }
 
-        private static bool SearchBlocked(MockNode node) => node.Key >= 10 || node.Key < 0;
+        [Test]
+        public void Result_Should_Contain_0() {
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            var count = 0;
+            foreach (var node in path) count++;
+            Assert.NotNull(path.GetNode(0));
+
+        }
+
+        [Test]
+        public void Result_Should_Not_Cointain_Minus1() {
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            Assert.Null(path.GetNode(-1));
+        }
+
+        [Test]
+        public void Result_Should_Not_Cointain_10() {
+            var path = _finder.Find(_pathMaker, _startKey, SearchBlocked, _graph);
+            Assert.Null(path.GetNode(10));
+        }
+
+        private static bool SearchBlocked(MockNode node) => node.Key > 9 || node.Key < 0;
 
         private class MockNode : IPathNode<int, MockNode> {
-            public int Key { get; set; } = 0;
-            public MockNode Previous { get; set; } = default;
-            public MockNode Next { get; set; } = default;
-            public float TraveledCost { get; set; } = default;
+            public int Key { get; set; }
+            public MockNode Previous { get; set; }
+            public MockNode Next { get; set; }
+            public float TraveledCost { get; set; }
             public MockNode(int key, MockNode previous) {
                 Key = key;
                 Previous = previous;
@@ -54,7 +85,10 @@ namespace AgnosticSearchTest {
 
         private class MockGraph : IGraph<int> {
             public ICollection<int>[] GetSurroundingNodes(int centre, int steps) {
-                return new List<int>[] { new List<int>() { centre }, new List<int>() { centre - 1, centre + 1 } };
+                return new List<int>[] { 
+                    new List<int>() { centre }, 
+                    new List<int>() { centre - 1, centre + 1 } 
+                };
             }
         }
 
@@ -72,11 +106,11 @@ namespace AgnosticSearchTest {
             }
 
             public IEnumerator<MockNode> GetEnumerator() {
-                return null;
+                return _nodes.GetEnumerator();
             }
 
             IEnumerator IEnumerable.GetEnumerator() {
-                return null;
+                return GetEnumerator();
             }
         }
     }
